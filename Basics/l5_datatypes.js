b3681990aee214437cd174c25c67a274711e9a29
Pratham Bhoriya <158551232+pbhoriya@users.noreply.github.com>
2024-07-08 14:24:00 +0000

    datatypes

---
+++ Basics/l5_datatypes.js
@@ -0,0 +1,15 @@
+let name="Pratham"
+let age=20
+let loggedIn=false
+let tools=null;
+let status;
+
+console.log(typeof "Pratham")
+console.log(typeof name)
+console.log(typeof 20)
+console.log(typeof age)
+console.log(typeof false)
+console.log(typeof loggedIn)
+console.log(typeof tools)
+console.log(typeof status)
+
