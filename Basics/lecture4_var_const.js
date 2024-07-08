2f51d8fd69fea483f59a0f81ea2f1777f1562c25
Pratham Bhoriya <158551232+pbhoriya@users.noreply.github.com>
2024-07-08 14:23:42 +0000

    var and const

---
+++ Basics/lecture4_var_const.js
@@ -0,0 +1,7 @@
+const accountId="122143"
+let accountName="Private"
+var accountmail="learning@mail.com"
+let accountType;
+accountCity="Jaipur"
+
+console.table([accountId,accountName,accountmail,accountType,accountCity])
