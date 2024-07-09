65fbd8681a8c967daae014c3f6a0b1e965083bb2
Pratham Bhoriya <158551232+pbhoriya@users.noreply.github.com>
2024-07-09 15:45:13 +0000

    type conversions

---
+++ Basics/l6_typeconversion.js
@@ -0,0 +1,26 @@
+//for numbers
+let score=33
+let score ="33abc"
+console.log(typeof(score))
+
+let valueInNumber=Number(score) //type conversion
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+33=33
+"33abc"=NaN
+null=0
+"abcd"=Nan
+undefined=Nan
+true=1 or false=0
+
+//for Boolean
+let login=1 // if there is no value then bool value will false and if there is a value then true
+let boollogin=Boolean(login)
+console.log(boollogin)
+
+//for strings
+let num=33
+let stringnumber=String(num)
+console.log(stringnumber)
+console.log(typeof stringnumber)
