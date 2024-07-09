6fe7d586071d883067e50699f9195aa18e825862
Pratham Bhoriya <158551232+pbhoriya@users.noreply.github.com>
2024-07-09 16:58:15 +0000

    stack and heap

---
+++ Basics/l10_stack_heap.js
@@ -0,0 +1,17 @@
+let myname="user"
+let username=myname
+console.log(username)
+username="chaiaurcode"
+console.log(username)
+
+let userone={
+    email:"ones@mail.com",
+    upi:"upi@upi.in",
+}
+
+let usertwo=userone
+
+usertwo.email="twos@mail.com"
+
+console.log(userone.email)
+console.log(usertwo.email)
