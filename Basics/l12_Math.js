2e5a870986a57d85c6ca5e99a153ceebec10e777
Pratham Bhoriya <158551232+pbhoriya@users.noreply.github.com>
2024-07-12 07:03:34 +0000

    math basic

---
+++ Basics/l12_Math.js
@@ -0,0 +1,19 @@
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(3.6))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.4))
+console.log(Math.min(2,4,6,8))
+console.log(Math.max(2,4,6,8))
+
+console.log(Math.random())// 0 and 1 ke beech value print krega
+console.log((Math.random()*10)+1)// 1 se value print krega
+console.log(Math.floor(Math.random()*10)+1)// decimal value print nhi krega
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()* (max-min+1)+min))// value 10 aur 20 k beech m print hogi
+
+
+
+
