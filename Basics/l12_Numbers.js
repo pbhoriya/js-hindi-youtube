23c594211ef23d33b41db335a21b318b4e74b311
Pratham Bhoriya <158551232+pbhoriya@users.noreply.github.com>
2024-07-12 06:17:25 +0000

    numbers

---
+++ Basics/l12_Numbers.js
@@ -0,0 +1,15 @@
+const num1=100
+console.log(num1)
+
+const balance= new Number(500) // object h
+console.log(balance)
+console.log(balance.toString())
+console.log(balance.toFixed(2))
+
+const num2=129.89
+console.log(num2.toPrecision(3))// round off krke value print krega =130
+
+const InNumber=1000000
+console.log(InNumber.toLocaleString('en-In'))
+
+
