41fb19f2f20c38643456fff71d382226e4950963
Pratham Bhoriya <158551232+pbhoriya@users.noreply.github.com>
2024-07-13 05:32:20 +0000

    date basics

--- l13_date.js
+++ l13_date.js
@@ -1,24 +1,24 @@
-let myDate= new Date()
-console.log(myDate)
-console.log(myDate.toString())
-console.log(myDate.toDateString())//shows only date
-console.log(myDate.toLocaleString())
-console.log(myDate.toISOString())
-console.log(typeof(myDate))
+// let myDate= new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())//shows only date
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toISOString())
+// console.log(typeof(myDate))
 
-let myCreatedDate=new Date(2024, 0, 13)
-console.log(myCreatedDate)
-console.log(myCreatedDate.toDateString())//shows only date
+// let myCreatedDate=new Date(2024, 0, 13)
+// console.log(myCreatedDate)
+// console.log(myCreatedDate.toDateString())//shows only date
 
-let anotherDate=new Date(2024,6,13,10,32)
-console.log(anotherDate.toString())
+// let anotherDate=new Date(2024,6,13,10,32)
+// console.log(anotherDate.toString())
 
-let anothersDate=new Date("2024-7-13")//YYYY-MM-DD
-console.log(anothersDate.toString())
+// let anothersDate=new Date("2024-7-13")//YYYY-MM-DD
+// console.log(anothersDate.toString())
 
 
-let anotherssDate=new Date("07-13-2024")//mm-dd-yyyy Indian Format
-console.log(anotherssDate.toString())
+// let anotherssDate=new Date("07-13-2024")//mm-dd-yyyy Indian Format
+// console.log(anotherssDate.toString())
 
 let aDate=new Date()
 console.log(aDate)
@@ -26,6 +26,10 @@ console.log(aDate.getMonth()+1)//Month 0 se start ho rha h
 console.log(aDate.getDate())
 console.log(aDate.getDay())
 console.log(aDate.getFullYear())
+console.log(aDate.toLocaleString('default',{
+    weekday:"long",
+    month:"long"
+}))
 
 
 
