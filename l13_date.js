6567d2b37a17276e710a34eb1f42b37e49739d3c
Pratham Bhoriya <158551232+pbhoriya@users.noreply.github.com>
2024-07-13 05:06:13 +0000

    date basics

---
+++ l13_date.js
@@ -0,0 +1,26 @@
+// let myDate= new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())//shows only date
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toISOString())
+// console.log(typeof(myDate))
+
+let myCreatedDate=new Date(2024, 0, 13)
+console.log(myCreatedDate)
+console.log(myCreatedDate.toDateString())//shows only date
+
+let anotherDate=new Date(2024,6,13,10,32)
+console.log(anotherDate.toString())
+
+let anothersDate=new Date("2024-7-13")//YYYY-MM-DD
+console.log(anothersDate.toString())
+
+
+let anotherssDate=new Date("07-13-2024")//mm-dd-yyyy Indian Format
+console.log(anotherssDate.toString())
+
+
+
+
+
