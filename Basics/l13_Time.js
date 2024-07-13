55450a2c72120f2eddefdc875e17368121a69b99
Pratham Bhoriya <158551232+pbhoriya@users.noreply.github.com>
2024-07-13 05:52:10 +0000

    moved

---
+++ Basics/l13_Time.js
@@ -0,0 +1,8 @@
+let myTimeStamp=Date.now()
+console.log(myTimeStamp)// Thu 1 1970 se ab tk ka time ms m print krega
+
+let aDate=new Date()
+console.log(aDate.getTime())
+console.log(aDate)
+
+console.log(Math.floor(Date.now()/1000))//ms into second
